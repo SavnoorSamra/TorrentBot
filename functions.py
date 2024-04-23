6ae7082f2d0f8aafc133355753f68009d5302c47
SavnoorSamra <samra.saab9876@gmail.com>
2024-04-23 01:15:46 -0700

    organized /downloading and /all added functions file

---
+++ functions.py
@@ -0,0 +1,20 @@
+from random import choice, randint
+def convertTime(seconds):
+    intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),)
+    if seconds!=8640000:
+        result = []
+        for name, count in intervals:
+            value = seconds // count
+            if value:
+                seconds -= value * count
+                if value == 1:
+                    name = name.rstrip('s')
+                result.append("{} {}".format(value, name))
+        return 'ETA: '+', '.join(result[:])
+    else:
+        return "ETA: N/A"
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord import Intents, Client, Message, app_commands
 from discord.ext import commands
 import qbittorrentapi
 import discord
+from functions import *
 
 
 load_dotenv()
@@ -11,77 +12,103 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 USERNAME = os.getenv('username')
 PASSWORD = os.getenv('password')
 HOST = os.getenv('host')
-
-# BOT SETUP
+PORT = os.getenv('port')
 
 intents = Intents.all()
 bot = commands.Bot(command_prefix='/', intents=intents)
-
-# MESSAGES
-
-# BOT PARAMETERS
 
 @bot.hybrid_command()
 async def hello(ctx: commands.Context):
     await ctx.send('bye')
 
 @bot.hybrid_command()
-async def torrents(ctx: commands.Context):
-    TorrentList = []
+async def downloading(ctx: commands.Context):
+    MovieList = []
+    TVList = []
+    OtherList = []
     count=0
-
-    #for torrent in qbt_client.torrents_info():
     for torrent in qbt_client.torrents.info.active():
-        count=count+1
-        TempList=[]
-        TempList.append(torrent.name + "\n" + convertTime(torrent.eta))
-        #TempList.append(torrent.category)
-        #TempList.append(str(round(torrent.progress*100,2))+"%")
-        #TempList.append(torrent.state)
-        #TempList.append(convertTime(torrent.eta))
-        TorrentList.append(TempList)
+        if torrent.category == 'radarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            MovieList.append(TempList)
+        if torrent.category == 'tv-sonarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            TVList.append(TempList)
+        if torrent.category != 'tv-sonarr' and torrent.category != 'radarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            OtherList.append(TempList)
 
     embed = discord.Embed(
-        title="**TORRENTS**",
+        title="**Torrents**",
         description="Currently Downloading Torrents",
         color=ctx.author.colour
     )
     if count == 0:
         await ctx.send("No Torrents Currently Downloading!")
     else:
-        for item in TorrentList:
-            embed.add_field(name="Name", value=item[0])
+        for item in MovieList:
+            embed.add_field(name="Movie", value=item[0])
+        for item in TVList:
+            embed.add_field(name="TV Show", value=item[0])
+        for item in OtherList:
+            embed.add_field(name="Other" ,value=item[0])
         await ctx.send(embed=embed)
 
-def convertTime(seconds):
-    intervals = (
-    ('weeks', 604800),  # 60 * 60 * 24 * 7
-    ('days', 86400),    # 60 * 60 * 24
-    ('hours', 3600),    # 60 * 60
-    ('minutes', 60),
-    ('seconds', 1),)
-    if seconds!=8640000:
-        result = []
-        for name, count in intervals:
-            value = seconds // count
-            if value:
-                seconds -= value * count
-                if value == 1:
-                    name = name.rstrip('s')
-                result.append("{} {}".format(value, name))
-        return 'ETA: '+', '.join(result[:])
+@bot.hybrid_command()
+async def all(ctx: commands.Context):
+    MovieList = []
+    TVList = []
+    OtherList = []
+    count=0
+    for torrent in qbt_client.torrents.info.all():
+        if torrent.category == 'radarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            MovieList.append(TempList)
+        if torrent.category == 'tv-sonarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            TVList.append(TempList)
+        if torrent.category != 'tv-sonarr' and torrent.category != 'radarr':
+            count = count+1
+            TempList = []
+            TempList.append(torrent.name + '\nProgress: ' + str(round(torrent.progress * 100, 2)) + "%" + '\n' + convertTime(torrent.eta))
+            OtherList.append(TempList)
+
+    embed = discord.Embed(
+        title="**Torrents**",
+        description="All Torrents",
+        color=ctx.author.colour
+    )
+    if count == 0:
+        await ctx.send("No Torrents Currently Downloading!")
     else:
-        return "N/A"
+        for item in MovieList:
+            embed.add_field(name="Movie", value=item[0])
+        for item in TVList:
+            embed.add_field(name="TV Show", value=item[0])
+        for item in OtherList:
+            embed.add_field(name="Other" ,value=item[0])
+        await ctx.send(embed=embed)
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} is now running!')
+    print(f'{bot.user} is now running! \nMade By @SavnoorSamra')
     await bot.tree.sync()
 
 # qBIT setup
 conn_info = dict(
     host=HOST,
-    port=8181,
+    port=PORT,
     username=USERNAME,
     password=PASSWORD
 )
@@ -96,17 +123,6 @@ print(f"qBittorrent: {qbt_client.app.version}")
 print(f"qBittorrent Web API: {qbt_client.app.web_api_version}")
 for k, v in qbt_client.app.build_info.items():
     print(f"{k}: {v}")
-
-@bot.event
-async def on_message(message: Message):
-    if message.author == bot.user:
-        return
-
-    username = message.author
-    user_message = message.content
-    channel = message.channel
-
-    print(username, user_message, channel)
 
 def main():
     bot.run(TOKEN)
